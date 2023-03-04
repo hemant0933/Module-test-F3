0161adf8aebb3cfd2e97a6a4c7e62c4b19d9452c
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-03-04 21:13:58 +0530

    loaded image

--- history/history.js
+++ history/history.js
@@ -5,20 +5,22 @@ function load() {
   var line;
 //   getting stored data from local storage;
   var MySearchdata = JSON.parse(localStorage.getItem("emptyarr"));
-  console.log(MySearchdata);
+    // console.log(MySearchdata);
   if (MySearchdata == null) {
     rows.innerHTML = "No history";
   } else {
+   
     MySearchdata.map((item) => {
-      line = `<div id='para'>  <div id='searchText'>${item.search}</div> 
-            &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <div id='dateTime'> ${item.time}</div>
-             </div>`;
+      line = `<div id='para'>
+            <div id='searchText'>${item.search}</div> 
+            &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 
+            <div id='dateTime'> ${item.time}</div>
+            </div>`;
     });
     rows.innerHTML = line;
   }
 }
 load();
-
 
 // code for clear button
 var clearbtn = document
--- script.js
+++ script.js
@@ -3,9 +3,9 @@
  let url = "https://www.googleapis.com/books/v1/volumes?q="
    
   var emptyarr = []
- 
+  var books = [];
   function search(){   
-    var books = JSON.parse(localStorage.getItem('emptyarr'))||[];
+  
 
     var filter = input.value.toUpperCase();
     var bookcontainer = document.getElementById('bookcontainer');
@@ -15,38 +15,38 @@
     .then((response) => response.json())
     .then((data) => {
         let bookdata = data.items;
-    
+        
+        // console.log(bookdata);
         books = bookdata.map((book) => {
             
             if (book.volumeInfo.title.toUpperCase().indexOf(filter) > -1) {
-                 
-                   let obj={
-                        search: filter,
-                        date:new Date().toLocaleDateString(),
-                        time:new Date().toLocaleDateString('en-us',{
-                            hour:'2-digit',
-                            minute:'2-digit',
-                        })
-
-                    }
-                    emptyarr.push(obj)
+                let booktitle=book.volumeInfo.title;  
+                let obj = {
+                    search: filter,
+                    date:new Date().toLocaleDateString(),
+                    time:new Date().toLocaleDateString('en-us',{
+                        hour:'2-digit',
+                        minute:'2-digit',
+                    }),
+                    bookName: booktitle,  
+                }
+                 emptyarr.push(obj)
             //  console.log("INSIDE MAP");
             return `<div class="card" id="card">
-                    <img src=${book.volumeInfo.imageLinks.thumbnail} data-img width="100%" alt="" srcset="">
+                    <img src=${book.volumeInfo.imageLinks.thumbnail}  width="100%" alt="img" srcset="">
                     <div class="book-title" data-header><strong>Title</strong> : ${book.volumeInfo.title}</div>
                     <div class="author" data-author><strong>Author </strong>: ${book.volumeInfo.authors}</div>
                     <div class="pagecount"><strong>Page Count </strong>: ${book.volumeInfo.pageCount}</div>
                     <div class="publisher"><strong>Publisher </strong>: ${book.volumeInfo.publisher}</div>
                     <div><button id="buynow">Buy Now</button></div>
                     </div>`
-            }
-            else{
-                JSON.parse(localStorage.getItem("emptyarr"))
-            }
+
+              }
+          
         });
         localStorage.setItem('emptyarr' , JSON.stringify(emptyarr))
         bookcontainer.innerHTML = books;
-    }).catch(error => console.error(error));
+    })
     
     }
 // this function will take me to the history page
