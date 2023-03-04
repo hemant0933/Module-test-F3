6441627c45663ea4479215dc60b9e32257fc14f3
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-03-04 21:43:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@
                  emptyarr.push(obj)
             //  console.log("INSIDE MAP");
             return `<div class="card" id="card">
-                    <img src=${book.imageLinks.thumbnail}  width="100%" alt="img" srcset="">
+                    <img src=${book.volumeInfo.imageLinks.thumbnail}  width="100%" alt="img" srcset="">
                     <div class="book-title" data-header><strong>Title</strong> : ${book.volumeInfo.title}</div>
                     <div class="author" data-author><strong>Author </strong>: ${book.volumeInfo.authors}</div>
                     <div class="pagecount"><strong>Page Count </strong>: ${book.volumeInfo.pageCount}</div>
